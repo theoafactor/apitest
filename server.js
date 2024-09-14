d380656d2cc58c60649c8cea96f998b8dd4d7174
Olu Adeyemo <theoafactor@gmail.com>
2024-09-14 03:33:40 +0100

    Added app submission endpoint

--- server.js
+++ server.js
@@ -15,5 +15,15 @@ server.get("/", (request, response)  => {
 })
 
 
+// checks for request
+server.get("/submit_application", (request, response) => {
+
+    response.send({
+        message: "Application submitted successfully!"
+    })
+
+
+})
+
 //listening to request 
 server.listen(process.env.PORT, () => console.log(`Server is listening on http://0.0.0.0:${process.env.PORT}`))
